a282ea8fc9e45e8d386d609456752b3c52ca8027
meIwata <waleilulu@gmail.com>
2025-04-15 18:26:05 +0800

    update

---
+++ varcomp/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a = 11; /*註解*/ 
+	int b = 3; /*宣告變數 帶初始值*/
+	printf("%d\n", a+b);
+	printf("%d\n", a-b);
+	printf("%d\n", a*b);
+	printf("%d\n", a/b);
+	printf("%d\n", a%b);
+	return 0;
+}
---
+++ varkind/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a = 5; /*俱计 跑计 盿﹍*/ 
+	int b; /*俱计 跑计*/
+	b = 3; /*倒*/ 
+	printf("%d\n", a);
+	printf("%d\n", b);
+	float c = 8.8;
+	printf("%f\n", c); /*计翴箇痙6*/
+	printf("%.2f\n", c); /*计翴2*/ 
+	char d = 'W'; /*じ*/
+	printf("%c\n", d); 
+	char e[]="James";
+	printf("%s\n", e);
+	double f = 8.8;
+	printf("%.1f\n", f);
+	return 0;
+}
