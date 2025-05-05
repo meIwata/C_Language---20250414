f05774be6bc08a979fe2ec54451b9c896010250a
meIwata <154941287+meIwata@users.noreply.github.com>
2025-05-05 20:43:20 +0800

    switch case

---
+++ swtest/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int num;
+	scanf("%d", &num);
+	switch(num){
+		case 1:
+			printf("One\n");
+			break;
+		case 2:
+			printf("Two\n");
+			break;
+		case 3:
+			printf("Three\n");
+			break;
+		case 4:
+			printf("Four\n");
+			break;
+		case 5:
+			printf("Five\n");
+			break;
+		default:
+			printf("Input error\n");
+	}
+	return 0;
+}
